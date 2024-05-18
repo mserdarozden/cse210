25e9224b77670a71ab7be7b61602cfdc948c7c51
mserdarozden <43015687+mserdarozden@users.noreply.github.com>
2024-05-18 20:51:27 +0300

    learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,12 +6,23 @@ class Program
     {
         Console.Clear();
 
-        List<int> randomNumbers = [];
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "", "European History The Causes of World War II by Mary Waters");
+    
+        string atext = assignment.GetSummary();
+        Console.WriteLine(atext);
+        Console.WriteLine("");
 
-        foreach (int randomNumber in randomNumbers)
-        {
-            Console.WriteLine(randomNumber);
-        }
-      
+        string b1text = mathAssignment.GetSummary();
+        string b2text = mathAssignment.GetHomeworkList();
+        Console.WriteLine(b1text);
+        Console.WriteLine(b2text);
+        Console.WriteLine("");
+
+        string ctext = writingAssignment.GetWritingInformation();
+        Console.WriteLine(ctext);
+    
+    
     }
 }
