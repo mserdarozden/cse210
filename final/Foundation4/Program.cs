3a18dd3501b134e8e7ed610934741a39189741d4
mserdarozden <43015687+mserdarozden@users.noreply.github.com>
2024-06-03 13:33:55 +0300

    foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("2024-06-3", 30, 3.0),
+            new Cycling("2024-06-3", 45, 12.0),
+            new Swimming("2024-06-3", 30, 20)
+        };
+    
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
